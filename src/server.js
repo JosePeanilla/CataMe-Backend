9b73f6d5c895cfc81e5b0ff9626c0bf1dffd2454
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-12-29 00:00:54 +0100

    Use the internal logger in the server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 require("dotenv").config()  /* Node module used to load environment variables from a .env file into process.env */
 
+/* Internal logger */
+const { Logger } = require("./utils/Logger.js")
+const logger = new Logger(__filename)
+
 const express = require("express")  /* Node module used to create an ExpressJS application */
 const cors = require("cors")  /* Node module used to permit CORS (Cross-Origin Resource Sharing) */
 
@@ -20,9 +24,9 @@ app.use(express.json())  /* Parse the incoming requests/responses in JSON payloa
 /* Express (HTTP) server listening for incoming requests */
 const server = app.listen(port, (error) => {
   if (error) {
-    console.error("[ERROR] The Express server couldn't get started!\n", error)
+    logger.error("The Express server couldn't get started!\n", error)
     return
   }
   /* The ExpressJS application is running, and ... */
-  console.log(`The HTTP server is listening for incoming requests on: http://localhost:${port}`)
+  logger.debug(`The HTTP server is listening for incoming requests on: http://localhost:${port}`)
 })
