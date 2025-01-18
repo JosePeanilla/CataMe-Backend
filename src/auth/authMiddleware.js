66ca6b0a0d8b1292010d36432269b90a514cdaa9
Jose Peanilla <josepeanilla@hotmail.com>
2025-01-18 23:34:40 +0100

    Eliminación de un fragmento repetido en el middleware de autentificación

--- src/auth/authMiddleware.js
+++ src/auth/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const { statusCodes } = require("../constants/statusCodes.js");
 
 const authMiddleware = {
-  verifyToken: async (req, res, next) => {
+  verifyToken: (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
@@ -13,16 +13,10 @@ const authMiddleware = {
     }
 
     const token = authHeader.split(" ")[1];
+
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decoded;
-      const user = await ConsumerUserModel.findById(decoded.id);
-      if (!user || !user.isActive) {
-        return res.status(statusCodes.Forbidden).send({
-          type: "error",
-          msg: "Access denied. User account is inactive.",
-        });
-      }
+      req.user = decoded; 
       next();
     } catch (error) {
       res.status(statusCodes.Forbidden).send({
