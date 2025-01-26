66685ce7c89653e22de134a0182b6678ea17a544
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-01-26 16:52:26 +0100

    Return winery users as well when calling the overall "users" GET method

--- src/users/usersService.js
+++ src/users/usersService.js
@@ -1,9 +1,13 @@
 const { ConsumerModel } = require("./consumers/ConsumerModel.js")  /* Model of 'consumer users' entity */
+const { WineryModel } = require("./wineries/WineryModel.js")  /* Model of 'winery users' entity */
 
 /* Service which interacts with the 'user' database */
 const usersService = {
   getAllUsers: async () => {
-    return { consumers: await ConsumerModel.find() }
+    return {
+      consumers: await ConsumerModel.find(),
+      wineries: await WineryModel.find()
+    }
   }
 }
 
